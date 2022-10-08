74c67e5dfa96e792cbb7d07388bcdf3482fc6f88
Geetanjali <asatigeetanjali@gmail.com>
2022-10-08 22:54:24 +0530

    first commit 8 oct

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import LandingPage from '../components/LandingPage';
+function Navs() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+            <LandingPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Navs;

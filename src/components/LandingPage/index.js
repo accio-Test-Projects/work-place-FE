c89dad2f1f5be126af2baf2aa2dc2868e34c1da6
sahil jaryal <sahil.jaryal@shiryam.com>
2022-10-12 18:19:43 +0530

    .

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Section2 from "./Section2/Section2";
-import Section3 from "./section-3/section.js"
 import BannerImage from './Section4'
 import Headers from "./Headers";
 function LandingPage() {

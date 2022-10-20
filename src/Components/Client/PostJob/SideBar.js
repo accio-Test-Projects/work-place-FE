383653315e0fa765a678750488cda3c1f4aa4603
Gyan Prakash Singh <gyangps1998@gmail.com>
2022-10-20 21:48:12 +0530

    realtimeupdatejobs

--- src/Components/Client/PostJob/SideBar.js
+++ src/Components/Client/PostJob/SideBar.js
@@ -5,17 +5,19 @@ function SideBar() {
   const [allJobs, setAllJobs] = useState(null);
   let loggedInUser = JSON.parse(localStorage.getItem("user"));
   let clientId = loggedInUser.uid;
-  const fetchAllJobs = async () => {
+  const fetchAllJobs =  () => {
     const q = query(collection(db, "jobs"), where("client_id", "==", clientId));
 
-    const querySnapshot = await getDocs(q);
-    let jobs = [];
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-      jobs.push(doc.data());
+    onSnapshot(q,querySnapshot=>{
+      let jobs = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        jobs.push(doc.data());
+      });
+      setAllJobs(jobs);
     });
-    setAllJobs(jobs);
+    
   };
   useEffect(() => {
     fetchAllJobs();
